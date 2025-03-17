802b348c14daa1b63378e6acd7f5a841919faab5
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-17 20:06:11 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿﻿string fraseDigitada, fraseComDislalia;
+
+Console.Write("   O rato roeu a roupa do rei de roma ");
+fraseDigitada = ("   O rato roeu a roupa do rei de roma ");
+
+fraseComDislalia = fraseDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(fraseComDislalia);
